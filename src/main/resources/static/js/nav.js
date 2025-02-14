c13826b3dcd45dd037476d495a9b72bec2236d04
Carlos Macián Gómez <133997397+carlosmaciangomez@users.noreply.github.com>
2025-02-14 16:28:37 +0100

    Version movil del index casi acabada, falta el desplegable del nav

---
+++ src/main/resources/static/js/nav.js
@@ -0,0 +1,7 @@
+document.getElementById('profileImage').addEventListener('click', function() {
+    window.location.href = '/perfil.html';
+});
+
+document.getElementById('nombreUsuarioNav').addEventListener('click', function() {
+    window.location.href = '/perfil.html';
+});
